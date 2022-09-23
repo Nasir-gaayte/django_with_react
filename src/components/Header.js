7fa9570a3e7033de13a3611265fda027c4f420a8
nasir-gaayte <slowboyz12@gmail.com>
2022-09-23 08:15:15 +0300

    install and site the react

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+
+import { Nav, Navbar, Container } from 'react-bootstrap'
+
+
+function Header() {
+  return (
+
+    <header>
+      <Container>
+        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+          <Container>
+            <Navbar.Brand href="/">Proshop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+
+    </header >
+
+  )
+}
+
+export default Header
